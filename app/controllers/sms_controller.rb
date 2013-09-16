768f066744e1318bd3f8e700448446d083d0b2b7
Alan Ruth <alanruth@live.com>
2013-09-15 22:23:43 -0600

    initial6

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -8,14 +8,14 @@ class SmsController < ApplicationController
 	  if @sms_state.nil? || @sms_state == ''
 		  @sms_state = 'step_1'
 		  session[:state] = @sms_state
-		  message = 'hello this is me'
-		  twiml = Twilio::TwiML::Response.new do |r|
-		    r.Sms message
-		  end
-		  twiml.text
+		  #message = 'hello this is me'
+		  #twiml = Twilio::TwiML::Response.new do |r|
+		  #  r.Sms message
+		  #end
+		  #twiml.text
 
-		  #message = '<Response><Sms>Thank you for contacting Sparktext. Please respond with your name.'+session[:state]+'</Sms></Response>'
-		  #render :text => message, :content_type => 'text/xml'
+		  message = '<Response><Sms>Thank you for contacting Sparktext. Please respond with your name.'+session[:state]+'</Sms></Response>'
+		  render :text => message, :content_type => 'text/xml'
 
 	  elsif @sms_state == 'step_1'
 		  @sms_state = "step_2"
